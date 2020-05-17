55cb06c2eed1e51942e1e6554d24c9cee9cced4f
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-17 09:17:44 +0530

    intro bundle of code- packages

---
+++ exercise/bundlesOfCode-Packages.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to bundles of code : Packages")
+
+	// If parts of your code are shared between multiple programs, you should consider moving them into packages.
+}
