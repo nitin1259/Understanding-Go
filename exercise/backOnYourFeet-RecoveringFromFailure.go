4cf5bf63e0acfa73d0876d86659911429cb74cbb
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-19 16:23:32 +0530

    panic and recover

--- exercise/backOnYourFeet-RecoveringFromFailure.go
+++ exercise/backOnYourFeet-RecoveringFromFailure.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	fmt.Println("back on your feet: Recovering from Failure")
 
+	// panic("oh no some wired happens")
+
 	// Deferring function calls
 	// The defer keyword ensures a function call takes place even if the calling function exits early, say, by using the return keyword.
 
@@ -25,6 +27,9 @@ func main() {
 	fmt.Println(sl)
 
 	scanDirectory("keyboard")
+
+	defer afterScanDirectory()
+	panicCreate()
 }
 
 func doSocial() error {
@@ -74,4 +79,48 @@ func scanDirectory(dirName string) error {
 		// fmt.Println(file.IsDir(), file.Name())
 	}
 	return nil
+}
+
+// Deferred calls completed before crash
+
+// recursive function to scan a directory for all files
+func scanDirectoryWithpanic(dirName string) {
+	fmt.Println(dirName)
+	fileArr, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		panic(err)
+	}
+	// retSl := make([]string, 0)
+	for _, file := range fileArr {
+		filepath := filepath.Join(dirName, file.Name())
+		if file.IsDir() {
+			err = scanDirectory(filepath)
+			if err != nil {
+				fmt.Println("error while reading directory %s error: %s", dirName, err)
+			}
+		} else {
+			fmt.Println(filepath)
+		}
+	}
+}
+
+func panicCreate() {
+	fmt.Println("Before panic")
+	panic(fmt.Errorf("Create err or from panicCreate"))
+	fmt.Println("after panic")
+}
+
+func afterScanDirectory() {
+	// Calling recover will not cause execution to resume at the point of the panic, at least not exactly.
+	// The function that panicked will return immediately, and none of the code in that function’s block following
+	// the panic will be executed. After the function that panicked returns, however, normal execution resumes.
+	p := recover()
+	if p == nil {
+		fmt.Println("no panic happens")
+	}
+
+	err, ok := p.(error)
+	if ok {
+		fmt.Println("happen becuase of err: ", err)
+	}
 }
