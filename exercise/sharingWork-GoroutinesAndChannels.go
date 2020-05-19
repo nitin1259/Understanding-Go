296efd1824b79ed430752ab38d3b2982493508d3
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-19 21:15:18 +0530

    go routine with channel

--- exercise/sharingWork-GoroutinesAndChannels.go
+++ exercise/sharingWork-GoroutinesAndChannels.go
@@ -5,24 +5,41 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
 	"time"
 )
 
 func main() {
 	fmt.Println("sharing work: Goroutines and Channels")
 
-	go a()
-	go b()
-	fmt.Println("main end")
-	time.Sleep(time.Second)
+	// go a()
+	// go b()
+	// fmt.Println("main end")
+	// time.Sleep(time.Second)
 
-	// responseSizeofWeb("http://www.example.com/")
-	// responseSizeofWeb("http://www.golang.org/")
-	// responseSizeofWeb("http://www.golang.org/doc")
+	go responseSizeofWeb("http://www.example.com/")
+	go responseSizeofWeb("http://www.golang.org/")
+	go responseSizeofWeb("http://www.golang.org/doc")
+	time.Sleep(time.Second * 5)
+
+	// channels
+	var myChannel chan float64
+	myChannel = make(chan float64)
+	// myChannel <- 3.14 // sending value to channel
+	// <-myChannel       // receiving value from channel
+	fmt.Println(reflect.TypeOf(myChannel), myChannel)
+
+	someChannel := make(chan string)
+
+	//Synchronizing goroutines with channels
+	go greetings(someChannel)
+	reciever := <-someChannel
+	fmt.Println(reciever)
 }
 
-func responseSizeofWeb(website string) {
-	response, err := http.Get(website)
+func responseSizeofWeb(url string) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,4 +61,8 @@ func b() {
 	for i := 0; i < 50; i++ {
 		fmt.Print("b")
 	}
+}
+
+func greetings(myChan chan string) {
+	myChan <- "Greetng from channels"
 }
