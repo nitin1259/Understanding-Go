72ea4f4d3e56ab03e0fcae714f954155f9c796aa
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-19 21:18:33 +0530

    go routine with channel

--- exercise/sharingWork-GoroutinesAndChannels.go
+++ exercise/sharingWork-GoroutinesAndChannels.go
@@ -35,6 +35,32 @@ func main() {
 	go greetings(someChannel)
 	reciever := <-someChannel
 	fmt.Println(reciever)
+
+	// another example
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	go abc(channel1)
+	go def(channel2)
+
+	fmt.Println(<-channel1)
+	fmt.Println(<-channel2)
+	fmt.Println(<-channel1)
+	fmt.Println(<-channel2)
+	fmt.Println(<-channel1)
+	fmt.Println(<-channel2)
+
+}
+
+func abc(chan1 chan string) {
+	chan1 <- "a"
+	chan1 <- "b"
+	chan1 <- "c"
+}
+
+func def(chan2 chan string) {
+	chan2 <- "d"
+	chan2 <- "e"
+	chan2 <- "f"
 }
 
 func responseSizeofWeb(url string) {
