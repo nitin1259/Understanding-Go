d76cbe3517066f9ace4af59c3744c502dc3f3086
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-21 15:55:23 +0530

    inserting data into template with action

--- exercise/aPatternToFollow-HtmlTemplates.go
+++ exercise/aPatternToFollow-HtmlTemplates.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
-	"net/http"
+	"os"
+	"text/template"
 )
 
 // a package that will load the HTML in from the file and insert signatures into it for us: the html/template package//
@@ -12,21 +12,42 @@ import (
 func main() {
 	fmt.Println("a pattern to follow: HTML Templates")
 
-	http.HandleFunc("/guestbook", viewHandler)
-	err := http.ListenAndServe("localhost:8090", nil)
-	log.Fatal(err)
+	// http.HandleFunc("/guestbook", viewHandler)
+	// err := http.ListenAndServe("localhost:8090", nil)
+	// log.Fatal(err)
+
+	// executeTemplate()
+
+	text := "Here is your template!!! and Action: {{.}} \n"
+	executeTemplateWithAction(text, "ABC")
 
 }
 
-func viewHandler(writer http.ResponseWriter, req *http.Request) {
+// func viewHandler(writer http.ResponseWriter, req *http.Request) {
 
-	// resp := []byte("Welcome to guestbook")
-	html, err := template.ParseFiles("./exercise/aPattern-view.html")
+// 	// resp := []byte("Welcome to guestbook")
+// 	html, err := template.ParseFiles("./exercise/aPattern-view.html")
+// 	checkError(err)
+
+// 	err = html.Execute(writer, nil)
+// 	checkError(err)
+
+// }
+
+func executeTemplate() {
+	text := "Here is your template!!!"
+	tmpl, err := template.New("test").Parse(text)
 	checkError(err)
-
-	err = html.Execute(writer, nil)
+	err = tmpl.Execute(os.Stdout, nil)
 	checkError(err)
+}
 
+func executeTemplateWithAction(text string, data interface{}) {
+	// text := "Here is your template!!!"
+	tmpl, err := template.New("test").Parse(text)
+	checkError(err)
+	err = tmpl.Execute(os.Stdout, data)
+	checkError(err)
 }
 
 func checkError(err error) {
