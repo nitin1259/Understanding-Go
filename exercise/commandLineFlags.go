9f59f59955f4831caca8765cc9162a940aae01ae
Nitin SINGH <nitinkumar.singh@f5.com>
2020-06-29 10:18:26 +0530

    some examples of commandline argument - flags

--- exercise/commandLineFlags.go
+++ exercise/commandLineFlags.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Command-Line Flags")
 
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numPtr := flag.Int("numb", 42, "some number")
+	boolPtr := flag.Bool("isAvailable", false, "some boolean flag")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string variable")
+
+	flag.Parse()
+
+	fmt.Println("workPtr: ", *wordPtr)
+	fmt.Println("numPtr: ", *numPtr)
+	fmt.Println("boolPtr: ", *boolPtr)
+
+	fmt.Println("svar: ", svar)
+
+	fmt.Println("tail: ", flag.Args())
 }
